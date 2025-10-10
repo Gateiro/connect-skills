b4676511b8ceb3a79295253d520f35d5675f4442
MATHEUS RODRIGUES VIEIRA <matheus.rvieira5@senacsp.edu.br>
2025-10-09 21:58:26 -0300

    Fomulário Login

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,23 @@
+/*
+
+Stack: container de navegação 
+(baseado no empilhamento de telas) => 
+push/replace/back
+
+1- router.push("/rota") => Empilha uma nova tela por cima da atual => usuário navega para
+a próxima tela, mas ainda pode voltar para a anterior.
+
+2- router.replace("/rota") => Substitui a tela atual, sem empilhar => quando você não quer 
+permitir voltar, como após o login (após se autenticar!!)
+
+3- router.back() => Remove o topo da pilha (volta uma tela) => quando o usuário clica em "voltar"
+
+*/
+
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+
   return <Stack />;
+  
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,59 @@
-import { Text, View } from "react-native";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import { Alert, SafeAreaView, Text, View } from "react-native";
 
-export default function Index() {
+
+export function Login() {
+  const router = useRouter();
+
+  //Infos de login pré-preenchidas (POR ENQUANTOOOO!!!)
+  const [email, setEmail] = useState('aluno@teste.com');
+  const [password, setPassword] = useState('123@senac');
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [loginError, setLoginError] = useState('');
+
+  // Verifica se e-mail e senha estão não vazios
+  const submit = email.trim() !== "" && password.trim() !== "" && !loading;
+
+  async function handleSignIn() {
+    try{
+      setLoading(true);
+      setLoginError('');
+      // Criar simulação (sem back) pra validar com os dados acima
+      /* Simula uma latência de rede (espera) de 600ms
+      new Promise((r) => setTimeout(r, 600)) cria uma promessa que 
+      resolve depois de 600ms; await pausa a função até passar este tempo*/
+      await new Promise((r) => setTimeout(r, 600));
+
+      if (email.toLowerCase() === "aluno@teste.com" && password === "123@senac") {
+        Alert.alert('Login simulado com sucesso');
+      }
+      else{
+        setLoginError('Email ou senhas inválidos');
+      }
+    }
+    finally{
+      setLoading(false);
+    }
+
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    //Front
+    <SafeAreaView>
+      {/*Formulário*/}
+      <View>
+        {/*Email*/}
+        <Text>E-mail</Text>
+        <TextInput 
+        placeholder="exemplo@email.com"
+        >
+        
+        </TextInput>
+      </View>
+
+    </SafeAreaView>
   );
+
 }
